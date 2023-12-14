5877ee7fcdb9cf257627fc263497d577cca9af61
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-14 19:38:12 +0530

    app server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import createHttpError, { isHttpError } from 'http-errors';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).jsonp({
+    message: 'Server is running',
+  });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-console.log('Express Server is running');
+import * as dotenv from 'dotenv';
+import app from './app';
+import mongoose from 'mongoose';
+
+// Configuration ENV
+dotenv.config();
+
+const PORT = process.env.PORT || 8004;
+const startApp = async () => {
+  try {
+    // Connect to MongoDB database
+    const connectionUrl = process.env.REMOTEURI ? process.env.REMOTEURI : '';
+    const conn = await mongoose.connect(connectionUrl);
+    console.log(`Connected to Remote Host : ${conn.connection.host}`);
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running under http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log('Unable to start App', error);
+    process.exit(1);
+  }
+};
+
+// Run the server once connected successfuly to remote db
+startApp();
