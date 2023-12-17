c59aa21cf10a4036bb06f16aa27059d594fcbded
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-17 09:19:34 +0530

    implement access token with create user item

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
 import * as UsersController from '../controllers/users';
+import { authVerify } from '../utils/common.util';
 
 const router = express.Router();
 
 // router.get('/', NotesController.getNotes);
 // router.get('/:noteId', NotesController.getNoteById);
-router.post('/', UsersController.createUser);
+router.post('/', authVerify, UsersController.createUser);
 router.post('/auth', UsersController.authUser);
 // router.put('/:noteId', NotesController.updateNote);
 // router.delete('/:noteId', NotesController.deleteNote);
--- src/utils/common.util.ts
+++ src/utils/common.util.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import createHttpError from 'http-errors';
 import { StatusCodes } from 'http-status-codes';
 import jwt from 'jsonwebtoken';
@@ -14,6 +15,26 @@ export function generateToken(payload: any, secretKey: any) {
   const token = jwt.sign({ payload }, secretKey, { expiresIn: '1h' });
   return token;
 }
+
+export const authVerify = (req: any, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization');
+  // console.log(token)
+  if (!token)
+    return res.status(StatusCodes.UNAUTHORIZED).send({
+      message: 'Access Denied',
+    });
+  try {
+    const secretkey = process.env.JWTKEY ? process.env.JWTKEY : '';
+    const verified = jwt.verify(token, secretkey);
+    // console.log(verified)
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(StatusCodes.NOT_ACCEPTABLE).send({
+      message: 'Invalid token',
+    });
+  }
+};
 
 export function checkEmpty(input: any) {
   if (typeof input === 'undefined' || input == null || input.length < 1) {
